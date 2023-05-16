156cbb0ef41816293afedf3209b65064ae443721
AlPussino <aungkhantkyaw8918@gmail.com>
2023-05-17 01:08:07 +0630

    second commit

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -21,42 +21,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            RichText(
-              text: const TextSpan(
-                text: 'By continuing, you are agreeing to our Terms of \n',
-                style: TextStyle(
-                  fontSize: 14.0,
-                  height: 1.5,
-                  color: Colors.black,
-                ),
-                children: [
-                  TextSpan(
-                    text: 'Service ',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.green,
-                    ),
-                  ),
-                  TextSpan(
-                    text: 'and ',
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black,
-                    ),
-                  ),
-                  TextSpan(
-                    text: 'Privacy Policy.',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.green,
-                    ),
-                  ),
-                ],
-              ),
-            ),
             ElevatedButton(
               onPressed: () {
                 final provider =
